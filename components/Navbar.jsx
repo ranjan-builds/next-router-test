02831bd82353cc3059aaa48067156732e7836d71
Ranjan Kashyap <ranjankashyap6475@gmail.com>
2025-08-09 19:26:50 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className=" fixed flex items-center gap-3 left-1/2 -translate-x-1/2  justify-center py-3 ">
       <div className=" bg-slate-100/10 rounded-full py-2 px-4 flex gap-2 ">
-        <Link href="/" className="py-1 px-2 hover:text-blue-400">
+        <Link href="Home" className="py-1 px-2 hover:text-blue-400">
           Home
         </Link>
         <Link className="py-1 px-2 hover:text-blue-400" href="About">
